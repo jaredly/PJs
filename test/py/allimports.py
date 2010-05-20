89a7068acbc2b88188de8628af0ec116004bb8d8
Jared Forsyth <jared@jaredforsyth.com>
2010-05-19 23:58:03 -0400

    adding python test files

---
+++ test/py/allimports.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from os.path import join as jon, dirname
+
+print jon('hi', 'ho')
+
+from os.path import *
+
+
+# vim: et sw=4 sts=4
---
+++ test/py/toimp.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+'''this is a module to bge imported'''
+
+thevalue = 20
+import example
+print __name__
+
+
+
+# vim: et sw=4 sts=4
