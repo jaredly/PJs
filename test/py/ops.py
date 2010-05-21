c7ab055c1035c067f36af8db6b383b16a8d4636f
Jared Forsyth <jared@jaredforsyth.com>
2010-05-21 14:37:06 -0400

    adding comparator tests

---
+++ test/py/ops.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+print 2==3
+print 2!=3
+print 2>3
+print 2<3
+print 2<=3
+print 2>=3
+
+print 1<2<3
+print 1==2==3
+print 1==1==1+1-1
+print 2!=3!=4
+
+print 'a'=='a'
+print 'a'=='b'
+print 'a'!='a'
+print 'a'!='b'
+
+
+# vim: et sw=4 sts=4
