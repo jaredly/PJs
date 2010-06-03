a2d51e04f818a87c918574c124b11fc6cd51e7f5
Jared Forsyth <jared@jaredforsyth.com>
2010-06-03 13:12:45 -0400

    adding pjs/ tests

---
+++ test/pjs/basic.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+x = 3
+
+assert(x == x)
+assert(3 == x)
+assert('Hello' == 'Hello')
+assert('Man' != 'Mouse')
+
+# vim: et sw=4 sts=4
---
+++ test/pjs/tojs.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+x = js([1,2,3,4])
+assert(js.x[1] == 2)
+assert(x.length == 4)
+assert(x.slice(0,2).length == 2)
+
+def m(e):
+    return js.e[0]
+def y():return 4
+z = js([m,y])
+
+assert [m][0] == m
+assert m == m
+assert js.z[0].__wraps__ == m
+
+assert js.z[0]([1]) == 1
+
+def a():
+    def b(a):
+        assert js.a[1] == 2
+    return b
+
+js.a()([1,2])
+
+assert(js.x.slice(2).slice(0)[1] == 4)
+
+# vim: et sw=4 sts=4
