94c07c892543ade75f53997b8f58d2de6bb33c77
Jared Forsyth <jared@jaredforsyth.com>
2013-05-18 20:37:55 -0600

    fixing setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
+#!/usr/bin/env python
 from distutils.core import setup
 import setuptools
 
 setup(name='PJs',
-      version='0.0.0',
+      version='0.0.1',
       description='kinda like pyjamas, but quicker, cleaner, and easier. has the goal of generating readable, usable *robust* javascript code from python code',
       author='Jared Forsyth',
       author_email='jared@jaredforsyth.com',
       url='http://jaredforsyth.com/projects/pjs/',
-      packages=['pjs', 'pjs.data', 'pjs.templates', 'jslib'],
+      packages=['pjs'],
+      install_requires=['pbj'],
       classifiers=['Programming Language :: Python :: 2.7']
      )
